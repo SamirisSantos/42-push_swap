a0d55f68313d45284ba98f3855d40115deae81aa
SamirisSantos <samiris.araujo@gmail.com>
2025-06-05 11:48:26 +0100

    update

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 12:29:50 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/06/04 16:28:40 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/06/05 11:45:51 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,14 @@ int	main(int argc, char *argv[])
 	b = NULL;
 	if (argc < 2)
 		return (1);
+	
 	while (i < argc)
 	{
 		nb = parser_int(argv[i]);
 		addl_stack(&a, nb);
 		i++;
 	}
+
 	printf("List:\n");
 	printList(a);
 	freeList(a);
