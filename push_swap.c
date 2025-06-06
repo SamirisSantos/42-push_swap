37b80fe8268b8011ce577db4f2e7464f6d322b88
Samiris De Araujo Santos <sade-ara@c2r1s11.42porto.com>
2025-06-06 15:33:21 +0100

    update

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sade-ara <sade-ara@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 12:29:50 by sade-ara          #+#    #+#             */
-/*   Updated: 2025/06/06 15:01:36 by sade-ara         ###   ########.fr       */
+/*   Updated: 2025/06/06 15:21:44 by sade-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(int argc, char *argv[])
 		addl_stack(&stack.a, nb);
 		i++;
 	}
-	//printList(stack.a);
+	printList(stack.a);
 	//printList(stack.b);
 	freeList(stack.a);
 	freeList(stack.b);
